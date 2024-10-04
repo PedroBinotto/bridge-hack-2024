305a5568eca946c7fe9888faca4031aff542ee16
Pedro Binotto <pedrosantibinotto@gmail.com>
2024-10-04 17:02:44 -0300

    aosdasodskl

--- backend/api/api.py
+++ backend/api/api.py
@@ -2,16 +2,6 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # Permitir todas as origens. Ajuste conforme necessário.
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 
 def train():
     print("tunando modelo...")
@@ -20,6 +10,18 @@ def train():
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     train()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas as origens. Ajuste conforme necessário.
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/bot_text/{text}")
